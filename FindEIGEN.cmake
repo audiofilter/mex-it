d3a16faedac8b2faa364d1cb45c836fa966100e4
Tony Kirke <tony@pyspuc.com>
2015-12-19 18:32:23 -0800

    Update FindEigen.cmake for local eigen3

--- FindEIGEN.cmake
+++ FindEIGEN.cmake
@@ -1,4 +1,4 @@
-find_path(EIGEN_INCLUDE_DIR Eigen HINTS "/usr/local/include/eigen3/" "/usr/include/eigen3/" ".." NO_DEFAULT_PATH)
+find_path(EIGEN_INCLUDE_DIR Eigen HINTS "/usr/local/include/eigen3/" "/usr/include/eigen3/" "./eigen3" NO_DEFAULT_PATH)
 set(EIGEN_INCLUDE_DIRS ${EIGEN_INCLUDE_DIR})
 
 include(FindPackageHandleStandardArgs)
